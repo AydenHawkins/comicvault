d441a7fd8651fd8461aa2d81e3476bad84627af2
HarrisonMoul <harrison.moul@icloud.com>
2025-03-16 12:40:45 -0400

    Collection Screen in an acceptable state.

--- comicvault/lib/collection_screen.dart
+++ comicvault/lib/collection_screen.dart
@@ -26,7 +26,7 @@ class CollectionScreen extends StatelessWidget {
           CollectionHeader(),
           Expanded(
             child: GridView.builder(
-              padding: const EdgeInsets.all(25),
+              padding: const EdgeInsets.only(left: 25, right: 25, top: 15),
               gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                 maxCrossAxisExtent: 200,
                 childAspectRatio: 2 / 3,
@@ -82,9 +82,73 @@ class CollectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-        
-      ],
+    return Padding(
+      padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+      child: Column(
+        children: [
+          TextFormField(
+            cursorColor: Colors.white,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: "ComicNeue",
+              fontSize: 20,
+            ),
+            decoration: InputDecoration(
+              labelText: "Search Collection",
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              labelStyle: TextStyle(fontFamily: "ComicNeue"),
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 110, 110, 110),
+                ),
+                onPressed: () {
+                  print(
+                    "Write a function to scan in comics. Probably screen transition.",
+                  );
+                },
+                child: Text(
+                  "Add Comic",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "ComicNeue",
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Spacer(),
+              ElevatedButton.icon(
+                icon: Icon(Icons.sort, color: Colors.white),
+                label: Text(
+                  "Sort",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "ComicNeue",
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 110, 110, 110),
+                ),
+                onPressed: () {
+                  print(
+                    "Probably open some dropdown menu with different sort options. Alphabetical for example.",
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
